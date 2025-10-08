96838d6d1b8743ac135a6f2aaaecc46cc599e966
lucasarruda12 <lucas123manoel4@gmail.com>
2025-10-08 07:59:28 -0300

    added plaintex macro(s)

---
+++ init.lua
@@ -0,0 +1,11 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+vim.opt.number = true
+vim.opt.scrolloff = 10
+
+vim.opt.termguicolors = false
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 4
+vim.opt.hlsearch = true
+
+require("config.mappings")
