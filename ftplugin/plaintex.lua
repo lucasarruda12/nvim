c482c743696be5c3ec2d5198e9c2437a282991a5
lucasarruda12 <lucas123manoel4@gmail.com>
2025-10-08 08:10:44 -0300

    moved everything back to the same file

--- ftplugin/plaintex.lua
+++
@@ -1,1 +0,0 @@
-vim.keymap.set("n", "<LocalLeader><LocalLeader>", ":w<cr> :silent !pdftex %<cr>")
--- init.lua
+++ init.lua
@@ -1,11 +1,21 @@
+-- <options>
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
+
 vim.opt.number = true
 vim.opt.scrolloff = 10
-
 vim.opt.termguicolors = false
 vim.opt.tabstop = 2
-vim.opt.shiftwidth = 4
+vim.opt.shiftwidth = 2
 vim.opt.hlsearch = true
+-- </options>
 
-require("mappings")
+-- <generic mappings>
+vim.keymap.set("i", "jk", "<esc>")
+vim.keymap.set("n", "<Leader>/", ":nohlsearch<cr>")
+-- </generic mappings>
+
+-- <plaintex mappings>
+vim.keymap.set("n", "<LocalLeader><LocalLeader>", ":w<cr> :silent !pdftex %<cr>")
+-- </plaintex mappings>
+
